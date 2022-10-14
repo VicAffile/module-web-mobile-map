4a805e78d3e424b3ad51974b2da8d9df542361a8
VicAffile <victoraffile@gmail.com>
2022-10-14 16:00:07 +0200

    map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
